7f59b707b5031245bf1bb5e675b00261ce6eb07b
Vil <vitalik.levchuk.008@gmail.com>
2024-12-28 09:40:00 +0300

    Add serializer class for improvements to the structural component of the project

---
+++ src/main/kotlin/org/fbs/al/util/Serializer.kt
@@ -0,0 +1,36 @@
+package org.fbs.al.util
+
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import java.io.File
+
+open class Serializer{
+
+    private val MAPPER = ObjectMapper()
+    private val XML_MAPPER = XmlMapper()
+
+    fun <T> serializeJson(obj: T, fileName: String) : File{
+        MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+        val file = File("$fileName.json")
+        MAPPER.writeValue(file, obj)
+        return file
+    }
+
+    fun <T> serializeXml(obj: T, fileName: String) : File{
+        XML_MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+        val file = File("$fileName.xml")
+        XML_MAPPER.writeValue(file, obj)
+        return file
+    }
+
+    fun registerModuleJsonMapper(module: Module) : Unit{
+        MAPPER.registerModules(module)
+    }
+
+    fun registerModuleXmlMapper(module: Module) : Unit{
+        XML_MAPPER.registerModules(module)
+    }
+
+}
